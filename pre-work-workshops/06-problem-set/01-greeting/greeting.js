213e31d5d1d0d1f9e0b34bdda54a86b994c04797
lishuweb <maharjanlishu@gmail.com>
2023-06-05 13:26:02 +0545

    greeting

--- pre-work-workshops/06-problem-set/01-greeting/greeting.js
+++ pre-work-workshops/06-problem-set/01-greeting/greeting.js
@@ -1,11 +1,4 @@
 // YOUR CODE BELOW
-// Define a function `greeting` that accepts an optional string argument called
-// name. `greeting` should return a personalized string if the name is present.
-
-// ```javascript
-// greeting('Sadie'); // => Hello Sadie!
-// greeting(); // => Hello!
-// ```
 
 function greeting(name)
 {
