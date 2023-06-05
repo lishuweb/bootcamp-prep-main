d0ddedfa823d79ee68cc75e9b69fbd4d1b6bbf3e
lishuweb <maharjanlishu@gmail.com>
2023-06-05 13:42:37 +0545

    mymnemonic

--- pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
+++ pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
@@ -1,13 +1,4 @@
 // YOUR CODE BELOW
-// Write a function `myMnemonic` that accepts up to four strings. `myMnemonic`
-// should return a new string of the first characters for each string argument.
-
-// Note that the user may not pass in all four strings!
-
-// ```javascript
-// myMnemonic('Must', 'Accept', 'Sugar', 'Happiness'); // => MASH
-// myMnemonic('We', 'Eat', 'Eggs'); // => WEE
-// ```
 
 function myMnemonic(str1,str2,str3,str4)
 {
