b6eb90581e13bac7cd6d02c4d9c2b08afa32d5b8
lishuweb <maharjanlishu@gmail.com>
2023-06-05 11:50:06 +0545

    defaultgreet

--- pre-work-workshops/05-functions/02-default-greet/default-greet.js
+++ pre-work-workshops/05-functions/02-default-greet/default-greet.js
@@ -1,1 +1,19 @@
 // YOUR CODE BELOW
+// Define a function, `defaultGreet`, that takes a first and a last name.
+// `defaultGreet` should return a string greeting for the given names. If the user
+// does not define a last name, assume the last name is 'Doe'.
+
+// ```javascript
+// defaultGreet('Chaka', 'Khan'); // => 'Hi Chaka Khan!'
+// defaultGreet('John'); // => 'Hi John Doe!'
+// ```
+
+function defaultGreet(firstName, lastName)
+{
+    if(lastName === undefined)
+    {
+        lastName = "Doe";
+    }
+    return "Hi" + " " +firstName + " " +lastName + "!";
+}
+defaultGreet();
