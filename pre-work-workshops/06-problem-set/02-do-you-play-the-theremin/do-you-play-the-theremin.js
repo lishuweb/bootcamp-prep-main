584db3aef5a43229c636450b7f1af0e34dbd3305
lishuweb <maharjanlishu@gmail.com>
2023-06-05 13:25:10 +0545

    doyouplaythetheremin

--- pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
+++ pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
@@ -1,12 +1,4 @@
 // YOUR CODE BELOW
-// Define a function `doYouPlayTheTheremin` that accepts a string as an argument.
-// `doYouPlayTheTheremin` should return true if the inputted string starts with the
-// letters `s` or `S`. Otherwise, `doYouPlayTheTheremin` should return false.
-
-// ```javascript
-// doYouPlayTheTheremin('Griffin'); // => false
-// doYouPlayTheTheremin('Scott'); // => true
-// ```
 
 function doYouPlayTheTheremin(string)
 {
