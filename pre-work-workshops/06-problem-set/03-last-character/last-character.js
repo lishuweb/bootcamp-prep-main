c64c24d2b33ab4be2b826596b070b082d3178e74
lishuweb <maharjanlishu@gmail.com>
2023-06-05 13:23:52 +0545

    lastCharacter

--- pre-work-workshops/06-problem-set/03-last-character/last-character.js
+++ pre-work-workshops/06-problem-set/03-last-character/last-character.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+
+function lastCharacter(string1, string2)
+{
+   if(string1[string1.length-1] === string2[string2.length-1])
+   {
+        return true;
+   }
+   else 
+   {
+        return false;
+   }
+}
+lastCharacter();
