73b470176b1a59c97245ab564e51f82f51c79a28
lishuweb <maharjanlishu@gmail.com>
2023-06-06 13:27:59 +0545

    crazycaps

--- workshops/02-loops/02-crazy-caps/crazy-caps.js
+++ workshops/02-loops/02-crazy-caps/crazy-caps.js
@@ -1,1 +1,24 @@
 // YOUR CODE BELOW
+
+function crazyCaps(string)
+{
+    debugger;
+
+    let newString = '';
+    for(i=0; i<string.length; i++)
+    {
+        let val = string[i];
+        if(i%2===1)
+        {
+            newString += val.toUpperCase();
+        }
+        else 
+        {
+            newString += val.toLowerCase();
+        }
+        
+    }
+    return newString;
+}
+console.log(crazyCaps("fullstack"));
+
