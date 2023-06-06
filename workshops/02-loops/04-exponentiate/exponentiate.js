1572bfd171d6d360675a4722f1fec486ae76ddd9
lishuweb <maharjanlishu@gmail.com>
2023-06-06 14:31:09 +0545

    exponentiate

--- workshops/02-loops/04-exponentiate/exponentiate.js
+++ workshops/02-loops/04-exponentiate/exponentiate.js
@@ -1,1 +1,25 @@
 // YOUR CODE BELOW
+function exponentiate(base, power)
+{
+    if(power >= 0)
+    {
+        if(power === 0)
+        {
+            return base = 1;
+        }
+        else 
+        {
+            let value = 1;
+            for(let i=1; i<=power; i++)
+            {
+                value *= base;
+            }
+            return value;
+        }
+    }
+    else 
+    {
+        return "Invalid power"; 
+    }
+}
+exponentiate();
