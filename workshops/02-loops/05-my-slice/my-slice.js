58b147e9e7c1a0830de9cdf84eb61de763c72e09
lishuweb <maharjanlishu@gmail.com>
2023-06-06 15:01:21 +0545

    myslice

--- workshops/02-loops/05-my-slice/my-slice.js
+++ workshops/02-loops/05-my-slice/my-slice.js
@@ -1,9 +1,28 @@
 // YOUR CODE BELOW
-function mySlice(string, startIdx, endIdx)
+function mySlice(originalString, startIdx, endIdx)
 {
-    
-    if(startIdx)
+    let newString = '';
+    if(startIdx === undefined && endIdx === undefined)
     {
-
+        return originalString;
     }
+
+    if(startIdx !== undefined && endIdx === undefined)
+    {
+        for(let i=startIdx; i<originalString.length; i++)
+        {
+            newString += originalString[i];
+        }
+        return newString; 
+    }
+    
+    if(startIdx !== undefined && endIdx !== undefined)
+    {
+        for(let i=startIdx; i<endIdx; i++)
+        {
+            newString += originalString[i];
+        }
+        return newString;
+    } 
 }
+mySlice();
