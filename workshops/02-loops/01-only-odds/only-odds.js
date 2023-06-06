f2785ce725e2a6dfdd9afbfa5fbbe046f5bfe2da
lishuweb <maharjanlishu@gmail.com>
2023-06-06 11:41:57 +0545

    onlyodds

--- workshops/02-loops/01-only-odds/only-odds.js
+++ workshops/02-loops/01-only-odds/only-odds.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+
+function onlyOdds(number)
+{
+    let sum = 0;
+    for (let i = number; i >= 1; i--) 
+    {
+        if (i % 2 === 1) 
+        {
+            sum += i;
+        }
+    }
+    return sum;
+}
+onlyOdds();
+
