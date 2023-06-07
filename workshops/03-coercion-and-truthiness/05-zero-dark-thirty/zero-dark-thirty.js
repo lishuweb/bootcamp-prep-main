0f9db12dbff6fd41f651c3815c3611af624721b9
lishuweb <maharjanlishu@gmail.com>
2023-06-07 15:03:35 +0545

    zerodarkthirty

--- workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
+++ workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
@@ -1,1 +1,24 @@
 // YOUR CODE BELOW
+function zeroDarkThirty(number)
+{
+    debugger;
+    if(number !== 0)
+    {
+        let newNumber = '';
+        number = String(number);
+        for(let i=0; i<number.length; i++)
+        {
+            if(number[i] === 0)
+            {
+                continue;
+            }
+            else 
+            {
+                newNumber += number[i];
+            }
+            return parseInt(newNumber);
+        } 
+    }
+    return NaN;
+}
+zeroDarkThirty();
