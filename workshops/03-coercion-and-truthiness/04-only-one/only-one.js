708797b60a4607b1adde1ed4e74817088f1911d2
lishuweb <maharjanlishu@gmail.com>
2023-06-07 13:55:45 +0545

    onlyone

--- workshops/03-coercion-and-truthiness/04-only-one/only-one.js
+++ workshops/03-coercion-and-truthiness/04-only-one/only-one.js
@@ -1,1 +1,18 @@
 // YOUR CODE BELOW
+function onlyOne(type1, type2, type3)
+{
+    if(type1 && !type2 && !type3)
+    {
+        return true;
+    }
+    if(!type1 && type2 && !type3)
+    {
+        return true;
+    }
+    if(!type1 && !type2 && type3)
+    {
+        return true;
+    }
+    return false;
+}
+onlyOne();
