5cb8c6e1a8098f90a38306f0993c04773ccdc6c7
lishuweb <maharjanlishu@gmail.com>
2023-06-08 11:43:00 +0545

    sniperscope

--- workshops/04-scope/04-sniper-scope/sniper-scope.js
+++ workshops/04-scope/04-sniper-scope/sniper-scope.js
@@ -1,58 +1,64 @@
 let bond = '007';
 
-function oddJob() {
+function oddJob() 
+{
   let AgentinScope = (bond === '007');
-  let prediction = null; // YOUR BOOLEAN PREDICTION HERE
+  let prediction = true; // YOUR BOOLEAN PREDICTION HERE
 
   return AgentinScope === prediction;
 }
 
 
-function goldFinger(bond) {
+function goldFinger(bond) 
+{
   let AgentinScope = (bond === '007')
-  let prediction = null; // YOUR BOOLEAN PREDICTION HERE
+  let prediction = false; // YOUR BOOLEAN PREDICTION HERE
 
   return AgentinScope === prediction;
 }
 
 
-function scaramanga(target) {
+function scaramanga(target) 
+{
   target = bond;
 
   let AgentinScope = (bond === '007')
-  let prediction = null; // YOUR BOOLEAN PREDICTION HERE
+  let prediction = true; // YOUR BOOLEAN PREDICTION HERE
 
   return AgentinScope === prediction;
 }
 
 
-function drNo() {
+function drNo() 
+{
   let bond = 'Body Double';
 
   let AgentinScope = (bond === '007');
-  let prediction = null; // YOUR BOOLEAN PREDICTION HERE
+  let prediction = false; // YOUR BOOLEAN PREDICTION HERE
 
   return AgentinScope === prediction;
 }
 
 
-function jaws(agent) {
+function jaws(agent) 
+{
   agent = bond;
   bond = 'Body Double';
 
   let AgentinScope = (agent === '007');
-  let prediction = null; // YOUR BOOLEAN PREDICTION HERE
+  let prediction = true; // YOUR BOOLEAN PREDICTION HERE
 
   return AgentinScope === prediction;
 }
 
 
-function elChiffre() {
+function elChiffre() 
+{
   let agent = bond;
   bond = 'Body Double';
 
   let AgentinScope = (agent === '007');
-  let prediction = null; // YOUR BOOLEAN PREDICTION HERE
+  let prediction = false; // YOUR BOOLEAN PREDICTION HERE
 
   return AgentinScope === prediction;
 }
