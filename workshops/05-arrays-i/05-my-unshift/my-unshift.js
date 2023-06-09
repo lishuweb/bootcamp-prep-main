cbcccc492177daa8e341caa1d0e5f45ddacab478
lishuweb <maharjanlishu@gmail.com>
2023-06-09 14:08:38 +0545

    myunshift

--- workshops/05-arrays-i/05-my-unshift/my-unshift.js
+++ workshops/05-arrays-i/05-my-unshift/my-unshift.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function myUnshift(array, newElement)
+{
+    let newArray = [newElement];
+    for(let i=0; i<array.length; i++)
+    {
+        let currentArray = array[i];
+        newArray.push(currentArray);
+    }
+    return newArray;
+}
+myUnshift([2,3,4,5], 1);
