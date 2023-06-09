830e67f887fef987926333c94667b74e9c0553b0
lishuweb <maharjanlishu@gmail.com>
2023-06-09 11:38:06 +0545

    oddcouple

--- workshops/05-arrays-i/01-odd-couple/odd-couple.js
+++ workshops/05-arrays-i/01-odd-couple/odd-couple.js
@@ -1,1 +1,19 @@
 // YOUR CODE BELOW
+function oddCouple(array)
+{
+    let newArray = [];
+    for(i=0; i<array.length; i++)
+    {
+        let num = array[i];
+        if(array[i]%2)
+        {
+            newArray.push(num);
+        }
+        if(newArray.length === 2) 
+        {
+            return newArray;
+        }
+    }
+    return newArray;
+}
+oddCouple();
--- workshops/06-arrays-ii/01-even-and-odd/even-and-odd.js
+++ workshops/06-arrays-ii/01-even-and-odd/even-and-odd.js
@@ -1,1 +1,28 @@
 // YOUR CODE BELOW
+// ### Even and Odd
+
+// Define a function, `evenAndOdd`, that accepts an array.
+
+// `evenAndOdd` should return a new array. The first element in the new array
+// should be an array with all of the even numbers from the original array. The
+// second element in the new array should be an array with all of the odd numbers
+// from the original array.
+
+// ```javascript
+// evenAndOdd([1, 2, 3, 4, 5, 6]); // => [[ 2 ,4, 6 ], [ 1, 3, 5 ]]
+// ```
+
+function evenAndOdd(arr)
+{
+    let arr1 = '';
+    let arr2 = '';
+    if(arr%2===0)
+    {
+        arr1 += arr;
+    }
+    else 
+    {
+        arr2 += arr;
+    }
+}
+evenAndOdd();
