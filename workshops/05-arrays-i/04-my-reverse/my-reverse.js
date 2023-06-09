1094a2d03795452695c99a0f706a6a20f85c8e45
lishuweb <maharjanlishu@gmail.com>
2023-06-09 13:53:23 +0545

    myreverse

--- workshops/05-arrays-i/04-my-reverse/my-reverse.js
+++ workshops/05-arrays-i/04-my-reverse/my-reverse.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+debugger;
+function myReverse(array)
+{
+    let newArray = [];
+    for(let i = array.length-1; i >= 0; i--)
+    {
+        // let currentArray = array[i];
+        newArray.push(array[i]);
+    }
+    return newArray;
+}
+myReverse([1,2,3,4]);
