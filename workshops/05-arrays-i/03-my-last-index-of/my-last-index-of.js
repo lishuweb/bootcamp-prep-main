bad6ca116c92541a6824d989761ca76769f9e5bd
lishuweb <maharjanlishu@gmail.com>
2023-06-09 13:15:44 +0545

    mylastindex

--- workshops/05-arrays-i/03-my-last-index-of/my-last-index-of.js
+++ workshops/05-arrays-i/03-my-last-index-of/my-last-index-of.js
@@ -1,1 +1,19 @@
 // YOUR CODE BELOW
+debugger;
+function myLastIndexOf(array, searchValue, startIdx)
+{
+    if(startIdx === undefined)
+    {
+       startIdx = array.length-1; 
+    }
+    for(let i=startIdx; i>=0; i--)
+    {
+        let value = array[i];
+        if(searchValue === value)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+myLastIndexOf();
