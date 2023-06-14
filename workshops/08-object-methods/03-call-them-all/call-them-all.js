b25f302fe9f33d1f90a289dcef70c14f59a887d3
lishuweb <maharjanlishu@gmail.com>
2023-06-14 14:00:01 +0545

    callthemall

--- workshops/08-object-methods/03-call-them-all/call-them-all.js
+++ workshops/08-object-methods/03-call-them-all/call-them-all.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+function callThemAll(obj, value)
+{
+    let finalArray = [];
+    // let currentKey = [];
+    for(let key in obj)
+    {
+        let currentKey = obj[key];
+        if(typeof currentKey === 'function')
+        {
+            let currentValue = currentKey(value);
+            finalArray.push(currentValue);
+        }
+    }
+    return finalArray;
+}
+callThemAll();
