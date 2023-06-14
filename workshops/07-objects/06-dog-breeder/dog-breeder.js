ff39a60cadf93f24c5d33dd9c5bacd16355657b0
lishuweb <maharjanlishu@gmail.com>
2023-06-14 08:11:30 +0545

    dogbreeder

--- workshops/07-objects/06-dog-breeder/dog-breeder.js
+++ workshops/07-objects/06-dog-breeder/dog-breeder.js
@@ -1,1 +1,30 @@
 // YOUR CODE BELOW
+function dogBreeder(name, age)
+{
+    let objectDog = {};
+    if(name === undefined)
+    {
+        name = "Steve";
+    }
+    if(age === undefined)
+    {
+        if(typeof name === 'number')
+        {
+            age = name;
+            name = 'Steve';
+        }
+        else 
+        {
+            age = 0;
+        }
+    }
+    
+    objectDog = {
+        name : name,
+        age: age
+    };
+
+    return objectDog;
+
+}
+dogBreeder();
