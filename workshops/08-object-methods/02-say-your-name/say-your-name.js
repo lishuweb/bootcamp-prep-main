9439bd12e8075337ce81d092a83827af2a3c3249
lishuweb <maharjanlishu@gmail.com>
2023-06-14 13:11:15 +0545

    sayyourname

--- workshops/08-object-methods/02-say-your-name/say-your-name.js
+++ workshops/08-object-methods/02-say-your-name/say-your-name.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+let me2 = {
+    name: "Lishu",
+    getGreeting: function(me)
+    {
+        return "Hi " + me.name + ", my name is " + this.name + ".";
+    }
+};
+
+
+debugger;
+me2.getGreeting("Liza");
+
