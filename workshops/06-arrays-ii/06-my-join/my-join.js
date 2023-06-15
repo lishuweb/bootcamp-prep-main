15ee2a0d8f60f9586cc3d44fe38d1abed36a86d3
lishuweb <maharjanlishu@gmail.com>
2023-06-15 08:19:00 +0545

    myjoin

--- workshops/06-arrays-ii/06-my-join/my-join.js
+++ workshops/06-arrays-ii/06-my-join/my-join.js
@@ -2,16 +2,23 @@
 function myJoin(array, separator)
 {
     let newString = '';
-    if(separator === '')
+    if(separator === undefined)
     {
         separator = ',';
     }
     for(let i=0; i<array.length; i++)
     {
         let currentArray = array[i];
-        newString += separator;
-        
-    }
+        if(i !== 0)
+        {
+            // newString.push(separator);
+            newString += separator;
+        }
+        if(currentArray !== null)
+        {
+            newString += currentArray;
+        }
+    }   
     return newString;
 }
 myJoin();
