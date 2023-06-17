13fdb7ba89a9cec6e01e91243615a83c4f17a2d1
lishuweb <maharjanlishu@gmail.com>
2023-06-17 22:02:36 +0545

    rotatearray

--- workshops/06-arrays-ii/08-rotate-array/rotate-array.js
+++ workshops/06-arrays-ii/08-rotate-array/rotate-array.js
@@ -1,1 +1,23 @@
 // YOUR CODE BELOW
+function rotateArray(originalArray, rotateNum)
+{
+    if(rotateNum > 0)
+    {
+        for(let i=0; i<rotateNum; i++)
+        {
+            let currentValue = originalArray.pop();
+            originalArray.unshift(currentValue);
+        }
+
+    }
+    else if(rotateNum < 0)
+    {
+        for(let i=0; i>rotateNum; i--)
+        {
+            let currentValue = originalArray.shift();
+            originalArray.push(currentValue);
+        }
+    }
+    return originalArray;
+}
+rotateArray();
