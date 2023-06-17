2b9f84d4ca46bcf39c795e5e52f4ca30b0c92937
lishuweb <maharjanlishu@gmail.com>
2023-06-17 14:58:01 +0545

    myjoin

--- workshops/06-arrays-ii/06-my-join/my-join.js
+++ workshops/06-arrays-ii/06-my-join/my-join.js
@@ -4,19 +4,20 @@ function myJoin(array, separator)
     let newString = '';
     if(separator === undefined)
     {
-        separator = ',';
+        separator = ",";
     }
     for(let i=0; i<array.length; i++)
     {
         let currentArray = array[i];
         if(i !== 0)
         {
-            // newString.push(separator);
             newString += separator;
+            // newString.push(separator);
         }
-        if(currentArray !== null)
+        if(currentArray !== undefined && currentArray !== null)
         {
             newString += currentArray;
+            // newString.push(separator);
         }
     }   
     return newString;
