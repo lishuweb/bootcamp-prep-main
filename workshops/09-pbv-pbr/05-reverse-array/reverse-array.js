ed55425e184cc45f18ee6f4c85ec0e05bbb92d21
lishuweb <maharjanlishu@gmail.com>
2023-06-18 22:19:06 +0545

    reversearray

--- workshops/09-pbv-pbr/05-reverse-array/reverse-array.js
+++ workshops/09-pbv-pbr/05-reverse-array/reverse-array.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+function reverseArray(array)
+{
+    let currentArray = [];
+    // if(array.length) 
+    while(array.length)
+    {
+        currentArray.push(array.pop());
+    }
+    while (currentArray.length) 
+    {
+        array.unshift(currentArray.pop())
+    }
+    return array;   
+}
+console.log(reverseArray([1,2,3,4]));
