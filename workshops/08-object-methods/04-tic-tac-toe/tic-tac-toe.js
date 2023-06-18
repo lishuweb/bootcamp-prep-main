4e7fb0f467ef5be63af8d6c27ec856743712463a
lishuweb <maharjanlishu@gmail.com>
2023-06-18 15:11:53 +0545

    tictactoe

--- workshops/08-object-methods/04-tic-tac-toe/tic-tac-toe.js
+++ workshops/08-object-methods/04-tic-tac-toe/tic-tac-toe.js
@@ -1,21 +1,4 @@
 // YOUR CODE BELOW
-// ### Tic Tac Toe
-
-// Define an object 'ticTacToe'.
-
-// Follow the test specs to create a 'board' property, 'move' method, and a 'clear'
-// method so you can play a game with your partner.
-
-// The 'board' property is a grid of arrays that represents the board, where each
-// cell is initiall null.
-
-// The 'move' method is a method that, given a character, rowNum, and colNum, adds
-// the character to the correct cell in the board.
-
-// The 'clear' method should reset the board to all nulls.
-
-// To make it easier to play, have your 'move' method console.log() the
-// board before the method returns the board.
 let ticTacToe = {
     board: [
         [null, null, null],
@@ -24,31 +7,23 @@ let ticTacToe = {
     ],
     move: function(character, rowNum, colNum)
     {
-        let final = ticTacToe.board;
-        let currentValue = [];
-        let data = [];
-        for(let i=0; i<rowNum; i++)
+        let currentCell = [];
+        currentCell = this.board[rowNum][colNum];
+        if (!currentCell) 
         {
-            for(let j=0; j<colNum; j++)
-            {
-                // data.push([i][j]);
-                let value = data(rowNum, colNum);
-                data.push(value);
-            }
-            final.push(data);
+            this.board[rowNum][colNum] = character;
         }
-        return final;
+        return this.board;
     }, 
 
     clear: function()
     {
-        board: [
+        return this.board = [
             [null, null, null],
             [null, null, null],
             [null, null, null]
         ]
-        return this.board;
+        // return this.board;
     }
 };
-
-console.log(move());
+move();
