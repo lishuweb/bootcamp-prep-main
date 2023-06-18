f213e1379902e6481cea74f6dce448e56d7c5a8c
lishuweb <maharjanlishu@gmail.com>
2023-06-18 22:26:03 +0545

    deepercopy

--- workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
+++ workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
@@ -1,1 +1,25 @@
 // YOUR CODE BELOW
+function deeperCopy(array)
+{
+    let newArray = [];
+    for(let i=0; i<array.length; i++)
+    {
+        let currentValue = array[i];
+        if(Array.isArray(currentValue))
+        {
+            let innerArray = [];
+            for(let j=0; j<currentValue.length; j++)
+            {
+                let currentValue1 = currentValue[j];
+                innerArray.push(currentValue1);
+            }
+            newArray.push(innerArray);
+        }
+        else 
+        {
+            newArray.push(currentValue);
+        }
+    }
+    return newArray;
+}
+deeperCopy();
