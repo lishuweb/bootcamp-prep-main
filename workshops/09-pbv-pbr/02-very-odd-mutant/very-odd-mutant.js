fcd3615bd5368b6138fc19c3422c9697bcc28040
lishuweb <maharjanlishu@gmail.com>
2023-06-18 14:04:09 +0545

    veryodd

--- workshops/09-pbv-pbr/02-very-odd-mutant/very-odd-mutant.js
+++ workshops/09-pbv-pbr/02-very-odd-mutant/very-odd-mutant.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+function veryOddMutant(array)
+{
+    let count = 0;
+    let mutatedArray = [];
+    for(let i=0; i<array.length; i++)
+    {
+        let currentNum = array[i];
+        if(!(currentNum % 2))
+        {
+            array[i] = 'normie';
+            // array.push(currentNum);
+            count++;
+        }
+    }
+    return count;
+}
