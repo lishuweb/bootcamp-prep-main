64514ecd7c06ea91d967887ba86fd285bc6fdfce
lishuweb <maharjanlishu@gmail.com>
2023-06-19 16:59:11 +0545

    getdogbreeder

--- workshops/11-higher-order-functions-ii/06-get-dog-breeder/get-dog-breeder.js
+++ workshops/11-higher-order-functions-ii/06-get-dog-breeder/get-dog-breeder.js
@@ -1,1 +1,31 @@
 // YOUR CODE BELOW
+function getDogBreeder(defaultName, defaultAge)
+{
+    return dogBreeder; 
+    function dogBreeder(name, age)
+    {
+        if(age === undefined)
+        {
+            if(typeof name === 'number')
+            {
+                age = name;
+                name = null;
+            }
+            else 
+            {
+                age = defaultAge;
+            }
+        }
+        if(!name)
+        {
+            name = defaultName;
+        }
+        let newValue = {
+            name: name,
+            age: age
+        };
+        return newValue; 
+    } 
+}
+
+
