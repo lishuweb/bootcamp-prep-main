cc4c8f6442ac3846d7d895a9d34b56451b4b91eb
lishuweb <maharjanlishu@gmail.com>
2023-06-19 14:16:50 +0545

    billerfunction

--- workshops/11-higher-order-functions-ii/01-biller-builder/biller-builder.js
+++ workshops/11-higher-order-functions-ii/01-biller-builder/biller-builder.js
@@ -1,1 +1,19 @@
 // YOUR CODE BELOW
+let NYFunc = function(price)
+{
+    return price * 1.03 * 1.04; 
+}
+
+let NJFunc = function(price)
+{
+    return price * 1.05 * 1.06625;
+}
+function billerBuilder(state)
+{
+    if(state === 'NY')
+    {
+        return NYFunc;
+    }
+    return NJFunc;
+}
+
