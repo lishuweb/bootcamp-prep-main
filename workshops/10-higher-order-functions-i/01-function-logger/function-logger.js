80cc7c80c1d8621db1e6fd17c5b02568938d4e0e
lishuweb <maharjanlishu@gmail.com>
2023-06-19 13:05:38 +0545

    functionlogger

--- workshops/10-higher-order-functions-i/01-function-logger/function-logger.js
+++ workshops/10-higher-order-functions-i/01-function-logger/function-logger.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+function functionLogger(callback, argument) 
+{
+    console.log('Function running');
+    let myFunc = callback(argument);
+  
+    console.log('Function complete');
+    return myFunc;
+}
+
+
