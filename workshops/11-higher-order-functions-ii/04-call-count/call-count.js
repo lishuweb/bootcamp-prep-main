bcf320c70f307f92c5ff0eb12e25351fb034d43b
lishuweb <maharjanlishu@gmail.com>
2023-06-19 15:04:18 +0545

    callcount

--- workshops/11-higher-order-functions-ii/04-call-count/call-count.js
+++ workshops/11-higher-order-functions-ii/04-call-count/call-count.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+// let count = 0;
+// count++;
+// callCount();
+
+function callCount()
+{
+    let count = 0;
+    return function()
+    {
+        count++;
+        return count;
+    }
+}
+
