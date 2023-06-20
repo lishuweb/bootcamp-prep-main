18bad9e991fe4e1b3cbff86afbdd6e6520849744
lishuweb <maharjanlishu@gmail.com>
2023-06-20 21:54:45 +0545

    cachesavings

--- workshops/11-higher-order-functions-ii/07-cache-savings/cache-savings.js
+++ workshops/11-higher-order-functions-ii/07-cache-savings/cache-savings.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+function cacheSavings(callback)
+{
+    let cache = 0;
+    let value;
+    return function newFunc(arg)
+    {
+        if(arg !== cache)
+        {
+            cache = arg;
+            value = callback(arg);
+            return value;
+        }
+        return value;
+    }
+}
