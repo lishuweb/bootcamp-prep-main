de76382bda79bd3f5f9af0e04ef226f62887a944
lishuweb <maharjanlishu@gmail.com>
2023-06-20 22:21:00 +0545

    myforeach

--- workshops/10-higher-order-functions-i/04-my-for-each/my-for-each.js
+++ workshops/10-higher-order-functions-i/04-my-for-each/my-for-each.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function myForEach(array, callback)
+{
+    for(let i=0; i<array.length; i++)
+    {
+        let currentArray = array[i];
+        callback(currentArray, i);
+        // callback(currentArray);
+    }
+}
