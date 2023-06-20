691fabb507c2eaec0155b247da560e367a93b77a
lishuweb <maharjanlishu@gmail.com>
2023-06-20 22:28:42 +0545

    chainreaction

--- workshops/10-higher-order-functions-i/05-chain-reaction/chain-reaction.js
+++ workshops/10-higher-order-functions-i/05-chain-reaction/chain-reaction.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function chainReaction(array, callback)
+{
+    // return array[0];
+    callback.forEach(function(callback)
+    {
+        array = callback(array);
+    })
+    return array;
+}
