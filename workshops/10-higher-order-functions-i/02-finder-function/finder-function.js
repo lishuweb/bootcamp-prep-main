0f15d2822210cace7b2af34905e2a465a505a6f4
lishuweb <maharjanlishu@gmail.com>
2023-06-20 22:04:08 +0545

    finderfunction

--- workshops/10-higher-order-functions-i/02-finder-function/finder-function.js
+++ workshops/10-higher-order-functions-i/02-finder-function/finder-function.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function finderFunction(array, callback)
+{
+    for(let i=0; i<array.length; i++)
+    {
+        let currentValue = array[i];
+        let element = callback(currentValue);
+        if(element)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
