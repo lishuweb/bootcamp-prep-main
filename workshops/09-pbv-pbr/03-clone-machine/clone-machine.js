a39ac54c724295987418a5117a502d100e85959c
lishuweb <maharjanlishu@gmail.com>
2023-06-20 22:38:22 +0545

    clonemachine

--- workshops/09-pbv-pbr/03-clone-machine/clone-machine.js
+++ workshops/09-pbv-pbr/03-clone-machine/clone-machine.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function cloneMachine(animal)
+{
+    let cloneObj = {
+        name: animal.name + 'Clone',
+        species: animal.species,
+        offspring: []
+    };
+    let value = cloneObj.name;
+    animal.offspring.push(value);
+    return cloneObj;
+}
