45aeb973568c10e7e1a3f0e04d769a98a29cb888
lishuweb <maharjanlishu@gmail.com>
2023-06-20 22:17:24 +0545

    whosaspecial

--- workshops/10-higher-order-functions-i/03-whos-a-special/whos-a-special.js
+++ workshops/10-higher-order-functions-i/03-whos-a-special/whos-a-special.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function whosASpecial(specialPets)
+{
+    let finalString = [];
+    specialPets.forEach(myPet);
+    function myPet(objPet)
+    {
+        finalString.push(`${objPet.name} the ${objPet.species} is very special!`);
+    }
+    return finalString.join(' ');
+   
+}
