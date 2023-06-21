0ec4ec4eb43fc360bbac9cb44df3da36102afbb7
lishuweb <maharjanlishu@gmail.com>
2023-06-21 19:51:32 +0545

    mostvowels

--- workshops/02-loops/07-most-vowels/most-vowels.js
+++ workshops/02-loops/07-most-vowels/most-vowels.js
@@ -1,22 +1,35 @@
 // YOUR CODE BELOW
+debugger;
 function mostVowels(str)
 {
-    str = str + " ";
-    let vowels = "AEIOUaeiou"
-    let currentChar, currentWord, currentVowelsCount;
-  let wordWithMostVowels = '', mostVowelsCount = 0;
-  for (let i = 0; i < str.length; i++) {
-    currentChar = str[i];
-    currentWord += currentChar;
-    if (vowels.includes(currentChar)) currentVowelsCount++;
-    if (currentChar === " ") {
-      if (currentVowelsCount > mostVowelsCount) {
-        wordWithMostVowels = currentWord;
-        mostVowelsCount = currentVowelsCount;
+    let vowels = "AEIOUaeiou";
+    let currentChar;
+    let currentWord = '';
+    let currentVowelsCount = 0;
+    let wordWithMostVowels = ''; 
+    let mostVowelsCount = 0;
+    for (let i = 0; i < str.length; i++) 
+    {
+      currentChar = str[i];
+      currentWord += currentChar;
+
+      if (vowels.includes(currentChar)) 
+      {
+        currentVowelsCount++;
       }
-      currentWord = "";
-      currentVowelsCount = 0;
-    }
+      
+      if (currentChar === " " || currentChar === '.' || currentChar === ',') 
+      {
+        if (currentVowelsCount > mostVowelsCount)
+        {
+          currentWord = currentWord.slice(0,-1);
+          wordWithMostVowels = currentWord;
+          mostVowelsCount = currentVowelsCount;
+          console.log(wordWithMostVowels);
+        }
+        currentWord = "";
+        currentVowelsCount = 0;
+      }
   }
   return wordWithMostVowels;
 }
