1e6a34be81f70367739c05a030baaca0fa1384f7
lishuweb <maharjanlishu@gmail.com>
2023-06-21 21:15:03 +0545

    myormyand

--- workshops/03-coercion-and-truthiness/03-my-or-my-and/my-or-my-and.js
+++ workshops/03-coercion-and-truthiness/03-my-or-my-and/my-or-my-and.js
@@ -1,12 +1,36 @@
 // YOUR CODE BELOW
 function myOr(type1, type2, type3)
 {
-    return type1 || type2 || type3;
+    if(type1)
+    {
+        return type1;
+    }
+    if(type2)
+    {
+        return type2;
+    }
+    if(type3)
+    {
+        return type3;
+    }
+    return type3;
 }
 myOr();
 
 function myAnd(type1, type2, type3)
 {
-    return type1 && type2 && type3;
+    if(!type1)
+    {
+        return type1;
+    }
+    if(!type2)
+    {
+        return type2;
+    }
+    if(!type3)
+    {
+        return type3;
+    }
+    return type3;
 }
 myAnd();
