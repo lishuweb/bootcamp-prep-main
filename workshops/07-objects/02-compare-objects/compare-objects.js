85acffe9cab49ddd498bc8f9aa869ad3893b6e06
lishuweb <maharjanlishu@gmail.com>
2023-07-06 08:38:39 +0545

    compareobjects

--- workshops/07-objects/02-compare-objects/compare-objects.js
+++ workshops/07-objects/02-compare-objects/compare-objects.js
@@ -1,35 +1,23 @@
 // // YOUR CODE BELOW
 function compareObjects(obj1, obj2)
 {
-    // let obj1Length = obj1.length;
-    // let obj2Length = obj2.length;
-
     let key1 = Object.keys(obj1);
     let key2 = Object.keys(obj2);
-    // let value1 =  =
     let value1 = [];
     let value2 = [];
-    for(let i in obj1)
+
+    if(key1.length !== key2.length)
     {
-        value1.push(obj1[i]);
+        return false;
     }
-    for(let i in obj2)
+    for(let i=0; i<key1.length; i++ )
     {
-        value2.push(obj2[i]);
-    }
-    debugger;
-    if(obj1.length === obj2.length)
-    {
-        for(let i=0; i<key1.length; i++ )
+        let val = key1[i];
+        if (obj1[val] !== obj2[val]) 
         {
-            if(key1[i] === key2[i] && value1[i] === value2[i])
-            {
-                return true;
-            }
             return false;
         }
     }
-    return false;
-
+    return true;
 }
-console.log(compareObjects({ name: 'nick' }, { name: 'nick' }));
+compareObjects();
