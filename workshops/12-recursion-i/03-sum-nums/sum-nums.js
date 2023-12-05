34619f39cf8aad8d2ff706ecb1458e20f44485d7
Lishu Maharjan <maharjanlishu@gmail.com>
2023-12-05 15:25:58 +0545

    sumNums-lishu.js

--- workshops/12-recursion-i/03-sum-nums/sum-nums.js
+++ workshops/12-recursion-i/03-sum-nums/sum-nums.js
@@ -1,1 +1,13 @@
-// YOUR CODE BELOW
+const sumNums = (num) => {
+    if(num <= 1) 
+    {
+       console.log(num);
+    } 
+    else 
+    {
+       num = num + sumNums(num - 1);
+       console.log(num);
+    }
+};
+
+sumNums(3);
