e4e3c1d98962346306a26e0ca74d5cec33dfc767
Lishu Maharjan <maharjanlishu@gmail.com>
2023-12-05 14:34:22 +0545

    backwardString-lishu.js

--- workshops/12-recursion-i/02-backward-string/backward-string.js
+++ workshops/12-recursion-i/02-backward-string/backward-string.js
@@ -1,1 +1,18 @@
 // YOUR CODE BELOW
+function backwardString(str)
+{
+    debugger
+    if(str.length === 1)
+    {
+        console.log(str);
+    }
+    else 
+    {
+        let lastChar = str[str.length - 1];
+        console.log(lastChar);
+        str = str.slice(0,-1);
+        backwardString(str);
+    }
+}
+
+backwardString("hello");
