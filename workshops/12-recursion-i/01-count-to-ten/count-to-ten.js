f366b920e224ecb343dc6c871f6b5889d011a9c8
Lishu Maharjan <maharjanlishu@gmail.com>
2023-12-05 10:56:02 +0545

    countToTen-lishu.js

--- workshops/12-recursion-i/01-count-to-ten/count-to-ten.js
+++ workshops/12-recursion-i/01-count-to-ten/count-to-ten.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+function countToTen(num)
+{
+    if(num >= 10)
+    {
+        console.log(num);
+    }
+    else 
+    {
+        console.log(num);
+        countToTen(num + 1);
+    }
+}
+
+countToTen(5);
