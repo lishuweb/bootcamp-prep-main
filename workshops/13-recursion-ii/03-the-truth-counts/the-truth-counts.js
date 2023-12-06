9cae0af69002d4f4d33f1ea85daffa14657db55c
Lishu Maharjan <maharjanlishu@gmail.com>
2023-12-06 16:10:12 +0545

    theTruthCounts-lishu.js

--- workshops/13-recursion-ii/03-the-truth-counts/the-truth-counts.js
+++ workshops/13-recursion-ii/03-the-truth-counts/the-truth-counts.js
@@ -1,1 +1,25 @@
 // YOUR CODE BELOW
+// theTruthCounts([0, [true, ['yes']]]); // => 2
+function theTruthCounts(arr)
+{
+    debugger;
+    let count = 0;
+    for(let i=0; i<arr.length; i++)
+    {
+        if(Array.isArray(arr[i]))
+        {
+            count += theTruthCounts(arr[i]);
+        }
+        else 
+        {
+            if(arr[i] !== 0)
+            {
+                count++;
+                // return count;
+            }
+        }
+    }
+    return count;
+}
+
+console.log(theTruthCounts([0, [true, ['yes']]]));
