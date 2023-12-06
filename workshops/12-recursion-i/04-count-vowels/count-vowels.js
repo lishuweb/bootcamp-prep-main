8239455b7172d1a286e9251cde84955aa775186b
Lishu Maharjan <maharjanlishu@gmail.com>
2023-12-06 13:30:31 +0545

    countVowels-lishu.js

--- workshops/12-recursion-i/04-count-vowels/count-vowels.js
+++ workshops/12-recursion-i/04-count-vowels/count-vowels.js
@@ -1,1 +1,21 @@
 // YOUR CODE BELOW
+function isBadVersion(n) 
+{
+  let leftVal = 1;
+  let rightVal = n;
+  while (leftVal < rightVal) 
+  {
+    let midVal = Math.floor((leftVal + rightVal) / 2);
+    if (isBadVersion(midVal)) 
+    {
+      rightVal = midVal;
+    } 
+    else 
+    {
+      leftVal = midVal + 1;
+    }
+    console.log(leftVal);
+  }
+};
+
+isBadVersion(5);
