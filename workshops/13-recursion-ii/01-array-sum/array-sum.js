1fcd0ea82771f03bdc9945ce4d0ee76f9680d339
Lishu Maharjan <maharjanlishu@gmail.com>
2023-12-06 13:35:03 +0545

    arraySum-lishu.js

--- workshops/13-recursion-ii/01-array-sum/array-sum.js
+++ workshops/13-recursion-ii/01-array-sum/array-sum.js
@@ -1,1 +1,21 @@
-// YOUR CODE BELOW
+// YOUR CODE BELOW\
+// arraySum([1, [2, 3, [4]]]) // => 10
+
+function arraySum(arr)
+{
+    let sum = 0;
+    for(let i=0; i<arr.length; i++)
+    {
+        if(Array.isArray(arr[i]))
+        {
+            sum += arraySum(arr[i]);
+        }
+        else 
+        {
+            sum += arr[i];
+        }
+    }
+    return sum;
+}
+
+console.log(arraySum([1, [2, 3, [4]]]));
