3e01813817c29015b9c9c94574ce6a80f3c11975
Lishu Maharjan <maharjanlishu@gmail.com>
2023-12-07 15:40:09 +0545

    getLongestName-lishu.js

--- workshops/13-recursion-ii/04-get-longest-name/get-longest-name.js
+++ workshops/13-recursion-ii/04-get-longest-name/get-longest-name.js
@@ -1,1 +1,43 @@
 // YOUR CODE BELOW
+let family = {
+    'Beverly Marquez': {
+        'Nina Rhone': {
+            'William Rhodes': null,
+            'Paul Nell': null,
+            'Sir Paddington the Fourth, of the county Wilstonshire': null
+        }
+    }
+};
+
+getLongestName(family); // => 'Sir Paddington the Fourth, of the county Wilstonshire'
+
+function getLongestName(family)
+{
+    let maxName = '';
+    for(let name in family)
+    {
+        if(name.length > maxName.length)
+        {
+            maxName = name;
+        }
+        if(family[name])
+        {
+            let subName = getLongestName(family[name]);
+            if(subName > maxName)
+            {
+                maxName = subName;
+            }
+        }
+    }
+    return maxName;
+}
+
+console.log(getLongestName(family = {
+    'Beverly Marquez': {
+        'Nina Rhone': {
+            'William Rhodes': null,
+            'Paul Nell': null,
+            'Sir Paddington the Fourth, of the county Wilstonshire': null
+        }
+    }
+}));
